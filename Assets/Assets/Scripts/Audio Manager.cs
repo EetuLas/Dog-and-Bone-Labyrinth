6a98ba3b14c49caf165b45d305066ec8f2814400
EetuLas <n3laee00@students.oamk.fi>
2025-08-29 10:49:42 +0300

    AudioMixer script completed
    For the script i took advice from my old project since the audio mixer script is pretty complicated. Now there is ambient noise playing when i press play button from the menu, i will add the volume sliders for master, sfx and music later.

--- Assets/Assets/Scripts/Audio Manager.cs
+++ Assets/Assets/Scripts/Audio Manager.cs
@@ -1,18 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] AudioMixer _mixer;
+    AudioMixerGroup _musicGroup;
+    AudioMixerGroup _sfxGroup;
+
+    [SerializeField] AudioClip _music;
+
+    const string MUSIC_GROUP_NAME = "Music";
+    const string SFX_GROUP_NAME = "SFX";
+
+    const string MASTER_VOLUME_NAME = "MasterVolume";
+    const string MUSIC_VOLUME_NAME = "MusicVolume";
+    const string SFX_VOLUME_NAME = "SFXVolume";
+
+    public static AudioManager Instance { get; private set; }
+
+    void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(this);
+        }
+        Instance = this;
+
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Init()
     {
-        
+        _musicGroup = _mixer.FindMatchingGroups(MUSIC_GROUP_NAME)[0];
+        _musicGroup = _mixer.FindMatchingGroups(SFX_GROUP_NAME)[0];
+
+        PlayAudio(_music, SoundType.Music, 1.0f, true);
+    }
+
+    public void ChangeMasterVolume(float volume)
+    {
+        _mixer.SetFloat(MASTER_VOLUME_NAME, Mathf.Log10(volume) * 20);
+    }
+
+    public void ChangeMusicVolume(float volume)
+    {
+        _mixer.SetFloat(MUSIC_VOLUME_NAME, Mathf.Log10(volume) * 20);
+    }
+
+    public void ChangeSFXVolume(float volume)
+    {
+        _mixer.SetFloat(SFX_VOLUME_NAME, Mathf.Log10(volume) * 20);
+    }
+
+    public enum SoundType
+    {
+        SFX,
+        Music
+    }
+
+    public void PlayAudio(AudioClip audioClip, SoundType soundType, float volume, bool loop)
+    {
+        GameObject newAudioSource = new(audioClip.name + " Source");
+        AudioSource audioSource = newAudioSource.AddComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.loop = loop;
+
+        switch (soundType)
+        {
+            case SoundType.SFX:
+                audioSource.outputAudioMixerGroup = Instance._sfxGroup;
+                break;
+            case SoundType.Music:
+                audioSource.outputAudioMixerGroup = Instance._musicGroup;
+                break;
+            default:
+                break;
+        }
+
+        audioSource.Play();
+
+        if (!loop)
+        {
+            Destroy(audioSource.gameObject, audioClip.length);
+        }
     }
 }
