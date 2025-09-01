5037136c4f6606fa90ebab41570ce3a61c37cdb6
EetuLas <n3laee00@students.oamk.fi>
2025-09-01 12:40:31 +0300

    Gameover screen
    Made the script for the gameoverscreen, the screen itself and now it triggers when the dog hits a wall.

--- Assets/Assets/Scripts/Walls.cs
+++ Assets/Assets/Scripts/Walls.cs
@@ -2,19 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Walls : MonoBehaviour
 {
+    [SerializeField] CanvasGroup _gameOverCG;
+
     void Start()
     {
-        
+        CanvasGroupSetState(_gameOverCG, false);
+    }
+
+    void CanvasGroupSetState(CanvasGroup canvasGroup, bool state)
+    {
+        canvasGroup.alpha = state ? 1.0f : 0.0f;
+        canvasGroup.interactable = state;
+        canvasGroup.blocksRaycasts = state;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Hit a wall!");
+            CanvasGroupSetState(_gameOverCG, true);
+            Time.timeScale = 0f;
         }
     }
 }
